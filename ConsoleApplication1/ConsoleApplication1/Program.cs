3722a52a5b948ffca2beebaa3966ae475f27835e
yyytiger <yyytiger@gmail.com>
2016-05-31 10:05:48 +0800

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            //comments from website
+            //comments from website 2
         }
     }
 }
